68f5303a764c97f52863c1b62dcb1c2faa3bd2ef
Man Hin <sitmanhin@gmail.com>
2021-06-05 20:34:55 +0800

    better message output

--- genplaylist.sh
+++ genplaylist.sh
@@ -1,54 +1,60 @@
 #!/usr/local/bin/bash
 
-lightblue="\033[96m"
+white="\033[0m"
+grey="\033[90m"
 red="\033[91m"
 green="\033[92m"
 yellow="\033[93m"
-white="\033[0m"
 blue="\033[94m"
-grey="\033[90m"
 purple="\033[95m"
+lightblue="\033[96m"
 
 Cprint() {
-    if [[ $3 == n ]]; then echo -ne ${!2}$1$white;
+    if [[ "$3" == + ]]; then echo -ne ${!2}$1$white;
     else echo -e ${!2}$1$white; fi
 }
 print() {
-    if [[ $2 == n ]]; then echo -ne $1;
-    else echo -e $1; fi
+    if [[ "$2" == + ]]; then echo -ne "$1";
+    else echo -e "$1"; fi
 }
 
 gen() {
     local inputDir=$@
     local currentDir=$(echo "$@" | sed 's#.*/##')
-    echo "$inputDir & $currentDir"
+    #echo "$inputDir & $currentDir"
     for i in "$inputDir"/*; do
         local item=$i
-        if [ -d "$item" ]; then
-            echo "Directory [[$item]] found, parsing..."
-            gen $item
-            echo "subfolder playlist ready, appending...$path/$(basename "$item").ppl to $currentDir.ppl"
-            cat "$path/$(basename "$item").ppl" >>"$currentDir.ppl"
+        if [ "$item" = "PLAYLISTS" ] || [ "$item" = "genplaylist.sh" ]; 
+            then continue
+        else 
+            if [ -d "$item" ]; then
+                print "Directory " +; Cprint "[$item]" lightblue +; print " found, parsing..."
+                gen $item
+                Cprint "subfolder playlist ready, appending $blue$path/$(basename "$item").ppl$green to $lightblue$currentDir.ppl$green" green
+                cat "$path/$(basename "$item").ppl" >>"$currentDir.ppl"
 
-        else
-            echo "Matching extension to corresponding digit..."
-            typeDIG=0
-            if [[ $(tr "[:upper:]" "[:lower:]" <<<"${item##*.}") = $(tr "[:upper:]" "[:lower:]" <<<"wav") ]]; then
-                typeDIG=2
-            elif [[ $(tr "[:upper:]" "[:lower:]" <<<"${item##*.}") = $(tr "[:upper:]" "[:lower:]" <<<"flac") ]]; then
-                typeDIG=4
-            fi
-            OUTpathname="${item#"$path"}"
-            OUTpathname="${OUTpathname////\\}"
-            if [ $typeDIG -ne 0 ]; then
-                echo "appending $item into $currentDir.ppl"
-                echo "\"E:$OUTpathname\"",$typeDIG,"\"\"","\"\"",$rand,0,$(($rand - 1)) >>"$currentDir.ppl"
             else
-                echo "The extension digit is unknown, skipping $(basename "$item")........."
+                print "$item"
+                Cprint "Matching extension to corresponding digit..." grey
+                typeDIG=0
+                if [[ $(tr "[:upper:]" "[:lower:]" <<<"${item##*.}") = $(tr "[:upper:]" "[:lower:]" <<<"wav") ]]; then
+                    typeDIG=2
+                elif [[ $(tr "[:upper:]" "[:lower:]" <<<"${item##*.}") = $(tr "[:upper:]" "[:lower:]" <<<"flac") ]]; then
+                    typeDIG=4
+                elif [[ $(tr "[:upper:]" "[:lower:]" <<<"${item##*.}") = $(tr "[:upper:]" "[:lower:]" <<<"m4a") ]]; then
+                    typeDIG=9
+                fi
+                OUTpathname="${item#"$path"}"
+                OUTpathname="${OUTpathname////\\}"
+                if [ $typeDIG -ne 0 ]; then
+                    Cprint "appending $item into $currentDir.ppl" grey
+                    echo "\"E:$OUTpathname\"",$typeDIG,"\"\"","\"\"",$rand,0,$(($rand - 1)) >>"$currentDir.ppl"
+                else
+                    Cprint "The extension digit of $purple$(basename "$item")$yellow is unknown, skipping" yellow
+                fi
+
             fi
-
         fi
-
     done
 }
 
